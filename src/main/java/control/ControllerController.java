9f6e97e7a598abcad7132c5d87bcc9979bb9c965
felix <fahrens@stud.hs-bremen.de>
2024-06-19 22:10:25 +0200

    Speichern eines Spielstandes implementiert

--- src/main/java/control/ControllerController.java
+++ src/main/java/control/ControllerController.java
@@ -1,6 +1,7 @@
 package control;
 
 import javafx.fxml.FXML;
+import model.GameFile;
 import res.Strings;
 
 import java.io.IOException;
@@ -22,6 +23,9 @@ public class ControllerController {
         SceneManager.goBack();
     }
 
-
+    @FXML
+    public void speichereSpielstand(){
+        GameFile.speichereSpielstand();
+    }
 
 }
