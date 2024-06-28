2ae07b51f02d70cfd74c95f2d51b717895401ca7
felix <fahrens@stud.hs-bremen.de>
2024-06-28 16:38:18 +0200

    Markt FAST FERTIG, weitere div. verbesserungen

--- src/main/java/model/Kaempfer.java
+++ src/main/java/model/Kaempfer.java
@@ -11,8 +11,7 @@ public class Kaempfer extends Charakter {
     private boolean istAmZug;
     private String imageURL;
 
-    public int getxPosition()
-    {
+    public int getxPosition() {
         return xPosition;
     }
 
@@ -65,8 +64,7 @@ public class Kaempfer extends Charakter {
      * @param zahlAusweichen
      * @param magieResistenz
      * @param bewegungsWeite
-     * @param initiative
-     * weitere parameter
+     * @param initiative      weitere parameter
      * @Author Felix Ahrens
      */
     public Kaempfer(String name, int gesundheit, int schild, int manapunkte, int nahkampfWert, int fernkampfWert,
@@ -91,7 +89,7 @@ public class Kaempfer extends Charakter {
                 Konstanten.INT_ZERO, Konstanten.INT_ZERO, istLeader, true, Strings.DATEIPFAD_ENDGEGNER);
     }
 
-    public static Kaempfer erstelleEndgegner () {
+    public static Kaempfer erstelleEndgegner() {
         return new Kaempfer(Strings.ENDGEGNER, Konstanten.INT_ONE_HUNDRED, Konstanten.INT_TEN, Konstanten.INT_TEN, Konstanten.INT_TEN, Konstanten.INT_TEN,
                 Konstanten.INT_TEN, Konstanten.INT_TEN, Konstanten.INT_TEN, Konstanten.INT_TEN, Konstanten.INT_TEN,
                 Konstanten.INT_ELEVEN, Konstanten.INT_ZERO, false, false, Strings.DATEIPFAD_ENDGEGNER);
