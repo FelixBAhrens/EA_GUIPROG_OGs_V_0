651f9dcd59b6b027ab85c23224fec2763ff69afc
felix <fahrens@stud.hs-bremen.de>
2024-07-06 15:09:49 +0200

    SCHMIEDE fertig

--- src/main/java/control/SchmiedenController.java
+++ src/main/java/control/SchmiedenController.java
@@ -1,13 +1,13 @@
 package control;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import model.Artefakt;
 import model.GameFile;
-import res.Konstanten;
 import res.Strings;
 
 
@@ -15,11 +15,21 @@ public class SchmiedenController extends PaneController
 {
     @FXML
     public AnchorPane artefaktDisplay;
+    @FXML
+    public Label infoLabel;
+    @FXML
+    public Label kostenLabel;
+    @FXML
+    public Button verbessernButton;
+    @FXML
+    public Button verkaufenButton;
+
 
     /**
      * Methode zum Anzeigen des Artefaktes
      *
      * @param event
+     * @Author Felix Ahrens
      */
     @FXML
     public void zeigeArtefaktDetails (MouseEvent event)
@@ -28,10 +38,27 @@ public class SchmiedenController extends PaneController
         artefaktDisplay.setVisible(true);
     }
 
+    /**
+     * Methode, die die auf dem "artefaktDisplay" ausgegebenen Informationen aktualisiert.
+     * @precondition
+     * @postcondition
+     * @param artefakt
+     * @Author Felix Ahrens
+     */
     public void aktualisiereDisplayInformation (Artefakt artefakt)
     {
-        Label label = (Label) artefaktDisplay.getChildren().get(Konstanten.INT_ONE);
-        label.setText(Strings.NAME + Strings.DOPPELPUNKT + Strings.SPACE + artefakt.getName() + Strings.NEWLINE
+        if (!artefakt.istImBesitz()){
+            verkaufenButton.setVisible(false);
+            verbessernButton.setVisible(false);
+        } else {
+            verbessernButton.setVisible(true);
+            verbessernButton.setText(Strings.STAERKE + Strings.SPACE +  Strings.VERBESSERN + Strings.DOPPELPUNKT + Strings.SPACE
+                    + artefakt.ermittleVerbesserungsKosten() + Strings.SPACE + Strings.GOLD);
+            verkaufenButton.setVisible(true);
+            verkaufenButton.setText(Strings.VERKAUFEN + Strings.DOPPELPUNKT + Strings.SPACE
+                    + artefakt.ermittleWert() + Strings.SPACE + Strings.GOLD);
+        }
+        infoLabel.setText(Strings.NAME + Strings.DOPPELPUNKT + Strings.SPACE + artefakt.getName() + Strings.NEWLINE
                 + Strings.IMBESITZ + Strings.DOPPELPUNKT + Strings.SPACE + artefakt.istImBesitz() + Strings.NEWLINE
                 + Strings.ANZAHL_ANWENDUNGEN + Strings.DOPPELPUNKT + Strings.SPACE + artefakt.getAnwendungenUebrig() + Strings.NEWLINE
                 + Strings.STAERKE + Strings.DOPPELPUNKT + Strings.SPACE + artefakt.getStaerke());
@@ -47,22 +74,13 @@ public class SchmiedenController extends PaneController
     public Artefakt findeArtefaktAusID (String ID)
     {
         GameFile instanz = GameFile.getInstanz();
-        switch (ID)
+        return switch (ID)
         {
-            case (Strings.SCHWERT_PANE):
-            {
-                return instanz.getSchwert();
-            }
-            case (Strings.STATUE_PANE):
-            {
-                return instanz.getStatue();
-            }
-            case (Strings.RING_PANE):
-            {
-                return instanz.getRing();
-            }
-        }
-        return null;
+            case (Strings.SCHWERT_PANE) -> instanz.getSchwert();
+            case (Strings.STATUE_PANE) -> instanz.getStatue();
+            case (Strings.RING_PANE) -> instanz.getRing();
+            default -> null;
+        };
     }
 
     /**
