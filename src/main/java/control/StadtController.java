32c1ee478b658651d451e32d128d1fe19a343aa1
felix <fahrens@stud.hs-bremen.de>
2024-07-13 18:35:20 +0200

    STADTCONTROLLER Fertig

--- src/main/java/control/StadtController.java
+++ src/main/java/control/StadtController.java
@@ -16,6 +16,19 @@ public class StadtController extends ControllerController
 {
     private Map<String, String> refMap = new HashMap<String, String>();
 
+    @FXML
+    private Pane gebaeudePane;
+
+    @FXML
+    private Pane hintergrundPane;
+
+    /**
+     * Methode, die die refMap konfiguriert, die eine Referenz zwischen den IDs der Panes und den zugehoerigen Dateinamen der FXML-Dateien der jeweiligen Gebaeude herstellt
+     * @pre Die Strings muessen im Interface "Strings" vorhanden sein. Die refMap muss in der Klasse StadtController existieren. Die refMap muss Werte vom Typ "String" sowohl als Key als auch als Value speichern koennen.
+     * @post Die "refMap" wurde initialisiert und mit den IDs der Panes als Keys und den Dateinamen der zu den IDs gehoerenden FXML-Dateien als Values gefuellt werden.
+     * @return die konfigurierte "refMap" die Strings und Strings enthaelt
+     * @Author Felix Ahrens
+     */
     public Map<String, String> setupRefMap (){
         refMap.clear();
         refMap.put(Strings.SCHMIEDE, Strings.FXML_SCHMIEDE);
@@ -29,13 +42,10 @@ public class StadtController extends ControllerController
         return refMap;
     }
 
-    @FXML
-    private Pane gebaeudePane;
-    @FXML
-    private Pane hintergrundPane;
-
     /**
      * Initialize-Methode der Klasse StadtController
+     * @pre
+     * @post
      * @Author Felix Ahrens
      */
     @FXML
@@ -46,7 +56,9 @@ public class StadtController extends ControllerController
     /**
      * Methode, die aus der zugehoerigen FXML-Datei aufgerufen wird und die Elemente der Pane
      *  sichtbar setzt. Die StyleClass der Pane wird nun auf einen nicht-transparenten tuerkisen Rand gesetzt.
-     * @param event
+     * @pre Das MouseEvent muss von einer Pane stammen. Die Methode muss von einer Pane aufgerufen werden, wenn ein Mauscursor ihr Gebiet betritt. Die genutzten Methoden und Variablen muessen existieren.
+     * @post Die Elemente der Pane und deren Rand wurden sichtbar und auf das Design aus der entsprechenden StyleClass gestellt.
+     * @param event Das Ereignis, das durch eine Mausaktion ausgeloest wurde und zum Methodenaufruf gefuehrt hat.
      * @Author Felix Ahrens
      */
     @FXML
@@ -67,7 +79,9 @@ public class StadtController extends ControllerController
      * Methode, die von einer Pane aufgerufen wird, wenn der Mauscursor die Pane verlaesst.
      *  Die Methode setzt die Elemente der Pane auf nicht visible und stellt den Hintergrund der Pane
      *  auf durchsichtig.
-     * @param event
+     * @pre Das MouseEvent muss von einer Pane stammen. Die Methode muss von einer Pane aufgerufen werden, wenn ein Mauscursor ihr Gebiet verlaesst. Die genutzten Methoden und Variablen muessen existieren.
+     * @post Die Elemente der Pane und deren Rand wurden unsichtbar und auf das Design aus der entsprechenden StyleClass gestellt.
+     * @param event Das Ereignis, das durch eine Mausaktion ausgeloest wurde und zum Methodenaufruf gefuehrt hat.
      * @Author Felix Ahrens
      */
     @FXML
@@ -75,7 +89,6 @@ public class StadtController extends ControllerController
     {
         Pane pane = (Pane) event.getSource();
         aendereStyleClass(pane, Strings.STYLECLASS_TRANSPARENT);
-
         for (javafx.scene.Node node : pane.getChildren())
         {
             if (node instanceof Button)
@@ -88,8 +101,11 @@ public class StadtController extends ControllerController
     /**
      * Methode zum aendern der StyleClass.
      *  Diese entfernt vorher alle StyleClasses und setzt die uebergebene StyleClass als einzige StyleClass.
-     * @param pane
-     * @param styleClass
+     * @pre Der Methode muessen eine Pane und ein String uebergeben werden. Die Pane muss eine "observableList" mit den styleClasses zurueckliefern.
+     *  Der uebergebene String muss einer vordefinierten StyleClass entsprechen.
+     * @post Die uebergebene Pane enthaelt nur noch die dieser Methode uebergebene styleClass
+     * @param pane Die Pane, die nur noch die uebergebene styleClass enthaelt.
+     * @param styleClass Die styleClass, die die Pane nur noch enthalten soll.
      * @Author Felix Ahrens
      */
     public void aendereStyleClass (Pane pane, String styleClass) {
@@ -102,20 +118,25 @@ public class StadtController extends ControllerController
      * Universelle Methode zum Oeffnen von Gebaeuden. Diese wird bei Mausklick auf eine der Panes
      *  aufgerufen und holt sich aus der ID der jeweiligen Pane mit der "refMap" den Pfad der FXML-Datei
      *  des jeweiligen Gebaeudes, um mit dieser (als String) die Methode "openGebaeude" aufzurufen.
-     * @param event
+     * @pre Das Event muss sich zu einer Pane typkonvertieren lassen und einer ID aus der refMap entsprechen.
+     *  "refMap.get()" muss einen String zurueckliefern und die genutzten Methoden muessen existieren
+     * @post Die Methode "oeffneGebaeude" der Klasse StadtController wurde mit einem String aus der "refMap" aufgerufen.
+     * @param event Das Ereignis, das durch eine Mausaktion ausgeloest wurde und zum Methodenaufruf gefuehrt hat.
      * @Author Felix Ahrens
      */
     @FXML
     public void handleGebaeudeOeffnen (MouseEvent event){
-        openGebaeude(refMap.get(((Pane)event.getSource()).getId()));
+        oeffneGebaeude(refMap.get(((Pane)event.getSource()).getId()));
     }
 
     /**
      * Methode, die ein Gebaeude in die Stadt reinlaedt und die entsprechende Controllerklasse setzt.
-     * @param fxmlFile
-     * @Author
+     * @pre Der uebergebene String fuer den Dateinamen einer FXML-Datei muss zu einer existierenden Datei passen.
+     * @post Die FXML-Datei, die dem uebergebenen String entspricht, wurde in die Pane "gebaeudePane" geladen und diese sichtbar gesetzt.
+     * @param fxmlFile die FXML-Datei des Gebaeudes, das in die Pane "gebaeudepane" reingeladen werden soll.
+     * @Author Felix Ahrens
      */
-    private void openGebaeude (String fxmlFile)
+    private void oeffneGebaeude (String fxmlFile)
     {
         try
         {
@@ -132,7 +153,14 @@ public class StadtController extends ControllerController
         }
     }
 
-    public void closeGebaeude ()
+    /**
+     * Methode zum schliessen des Gebaeudes. Die Panes "gebaeudePane" und "hintergrundPane" werden auf invisible
+     *  gesetzt. Alles, was in der gebaeudePane enthalten war - also das geladene Gebaeude wird entfernt.
+     * @pre Die Panes "gebaeudePane" und "hintergrundPane" muessen in der Klasse existieren
+     * @post Die Panes wurden auf unsichtbar gesetzt und jeder Inhalt der "gebaeudePane" entfernt.
+     * @Author Felix Ahrens
+     */
+    public void schliesseGebaeude ()
     {
         gebaeudePane.setVisible(false);
         hintergrundPane.setVisible(false);
