e7cd39bb169974cf4d6d9a9b825e1e2639dd5517
felix <fahrens@stud.hs-bremen.de>
2024-07-17 20:03:31 +0200

    doku

--- src/main/java/control/EinheitenController.java
+++ src/main/java/control/EinheitenController.java
@@ -32,9 +32,69 @@ public class EinheitenController extends ControllerController
     public VBox charakterDisplay;
 
     /**
+     * Initialize-Methode des EinheitenControllers. Hier werden Methoden aufgerufen, die zum Initialisieren
+     *  der Funktionalitaet der Klasse benoetigt werden. Dazu gehoert etwa die Methode "erstelleDefaultCharakterHashMap", die
+     *  eine HashMap mit Verbindungen zwischen Instanzen der Klasse Charakter und Farb-Strings herstellt.
+     * @pre Die Methoden und die Klasse muessen existieren.
+     * @post Die fuer die weitere Nutzung der Klasse als Controllerklasse der zugehoerigen FXML-Datei benoetigten Methoden
+     *  zum initialisieren wurden aufgerufen.
+     * @Author Felix Ahrens
+     */
+    @FXML
+    public void initialize ()
+    {
+        CharakterController.erstelleDefaultCharakterHashMap();
+        aktualisiereEinheitenHBox();
+    }
+
+    /**
+     * Methode, die von GUI-Elementen in der FXML-Datei aufgerufen wird. Diese ruft die benoetigten Methoden auf,
+     *  um das Waehlen eines Charakters logisch umzusetzen. Der zugehoerige Charakter wird auf einem Stack abgelegt und
+     *  die Displays aktualisiert.
+     * @pre Die verwendeten Methoden und Klassen muessen existieren. Die ID, die aus dem MouseEvent gezogen wurde,
+     *  muss den unter "macheCharakterAusId" spezifizierten Konventionen entsprechen.
+     * @post Das Waehlen des Charakters wurde gespeichert und visuell bestaetigt.
+     * @param mouseEvent Das Event, aus dem der Methodenaufruf stammt.
+     * @Author Felix Ahrens
+     */
+    @FXML
+    public void waehleCharakter (MouseEvent mouseEvent)
+    {
+        charakterStack.add(macheCharakterAusID(((AnchorPane)mouseEvent.getSource()).getId()));
+        ((AnchorPane) mouseEvent.getSource()).setDisable(true);
+        aktualisiereEinheitenHBox();
+        aktualisiereMissionsEinheitenHBox();
+    }
+
+    /**
+     * Methode, die aus einer ID, wie sie in der FXML-Datei zahlreich vorhanden ist, den zugehoerigen Charakter waehlt und zurueckgibt.
+     *  Dabei wird nur der hinter dem Unterstrich stehende Teil der ID (des uebergebenen Strings) betrachtet. Je nach dessen Name
+     *  wird der jeweilige Getter des zugehoerigen Charakters aus der Klasse GameFile aufgerufen
+     * @pre Die ID darf nur einen Unterstrich enthalten. Hinter dem Unterstrich darf nur noch der Name des Zugehoerigen
+     *  Charakters stehen, etwa "Leader". Dabei muss der erste Buchstabe gross geschrieben sein. Die ID darf hinter dem Bruchstrich nur
+     *  einen der fuenf Charakternamen enthalten. Die verwendeten Interfaces, Klassen und Methoden muessen vorhanden und erreichbar sein.
+     * @post Es wurde ein Charakter zurueckgegeben, der der ID entsprach.
+     * @param id Die ID des GUI-Elements, mit dem diese Methode einen Charakter auswaehlen soll.
+     * @return Die zur ID passende Instanz der Klasse Charakter
+     * @Author Felix Ahrens
+     */
+    public Charakter macheCharakterAusID (String id){
+        GameFile instanz = GameFile.getInstanz();
+        return switch(id.split(Strings.UNTERSTRICH)[Konstanten.INT_ONE])
+        {
+            case Strings.LEADER -> instanz.getLeader();
+            case Strings.MEDIC -> instanz.getMedic();
+            case Strings.HUNTER -> instanz.getHunter();
+            case Strings.MAGICIAN -> instanz.getMagician();
+            case Strings.SCOUT -> instanz.getScout();
+            default -> null;
+        };
+    }
+
+    /**
      * Methode, die die EinheitenHBox aktualisiert
-     * @pre
-     * @post
+     * @pre Die verwendeten Methoden, Klassen und Interfaces muessen existieren und erreichbar sein. Die Instanz der Klasse Charakter muss die verwendeten Methoden und Parameter haben.
+     * @post Die EinheitenHBox wurde visuell auf den neuesten Stand gebracht und zeigt (nun wieder) die fuer die nutzende Person wichtigen Informationen an und ermoeglicht die Interaktion mit dieser.
      * @Author Felix Ahrens
      */
     public void aktualisiereEinheitenHBox () {
@@ -49,56 +109,16 @@ public class EinheitenController extends ControllerController
     }
 
     /**
-     * Initialize-Methode des EinheitenControllers.
-     * @pre
-     * @post
+     * Methode, die die "MissionsEinheitenHBox" aktualisiert. Dazu werden erst alle Children, also der gesamte Inhalt, invisible
+     *  gesetzt. Danach wird der "charakterStack" durchgegangen. Fuer jeden Charakter in diesem wird die zugehoerige
+     *  AnchorPane auf visible gesetzt.
+     * @pre Die verwendeten Klassen, Methoden und Interfaces (Konstanten) muessen verfuegbar und erreichbar sein.
+     * @post Die HBox "MissionsEinheitenHBox" wurde aktualisiert, dabei werden nur die ausgewaehlten, also im "charakterStack" liegenden Charakter-AnchorPanes angezeigt
      * @Author Felix Ahrens
      */
-    @FXML
-    public void initialize ()
-    {
-        CharakterController.erstelleDefaultCharakterHashMap();
-        aktualisiereEinheitenHBox();
-    }
-
-
-    /**
-     * Methode, die durch einen Button-click aufgerufen wird und den gesetzten Kampf mit den ausgewaehlten Einheiten startet.
-     * @pre
-     * @post
-     * @Author Felix Ahrens
-     */
-    @FXML
-    public void starteMission(){
-        ObservableList<Node> alleCharaktere = MissionsEinheitenHBox.getChildren();
-    }
-
-    @FXML
-    public void waehleCharakter (MouseEvent mouseEvent)
-    {
-        charakterStack.add(macheCharakterAusID(((AnchorPane)mouseEvent.getSource()).getId()));
-        ((AnchorPane) mouseEvent.getSource()).setDisable(true);
-        aktualisiereEinheitenHBox();
-        aktualisiereMissionsEinheitenHBox();
-    }
-
-    public Charakter macheCharakterAusID(String id){
-        GameFile instanz = GameFile.getInstanz();
-        return switch(id.split(Strings.UNTERSTRICH)[Konstanten.INT_ONE])
-        {
-            case Strings.LEADER -> instanz.getLeader();
-            case Strings.MEDIC -> instanz.getMedic();
-            case Strings.HUNTER -> instanz.getHunter();
-            case Strings.MAGICIAN -> instanz.getMagician();
-            case Strings.SCOUT -> instanz.getScout();
-            default -> null;
-        };
-    }
-
     public void aktualisiereMissionsEinheitenHBox ()
     {
         MissionsEinheitenHBox.getChildren().forEach(node -> node.setVisible(false));
-        System.out.println(charakterStack.size());
         for (Charakter charakter : charakterStack) {
             MissionsEinheitenHBox.getChildren().get(switch (charakter.getName()){
                 case Strings.LEADER -> Konstanten.INT_ZERO;
@@ -111,12 +131,26 @@ public class EinheitenController extends ControllerController
         }
     }
 
+    /**
+     * Methode zum Fortfahren. Diese setzt das "EinheitenArray" in der Klasse GameFile auf den Inhalt des charakterStacks.
+     * @pre Die Singleton-Instanz der Klasse GameFile muss gesetzt sein. Die Methoden und Klassen muessen vorhanden und erreichbar sein.
+     * @post Der charakterStack wurde im "einheitenArray" in der Klasse GameFile gespeichert und die Szene "Kampf" wurde geladen.
+     * @Author Felix Ahrens
+     */
     @FXML
     public void fahreFort (){
         GameFile.getInstanz().setEinheitenArray(charakterStack.toArray(new Charakter[charakterStack.size()]));
         SzenenManager.wechseleSzene(Strings.FXML_KAMPF);
     }
 
+    /**
+     * Methode, die aus GUI-Elementen der zugehoerigen FXML-Datei aufgerufen wird und die dem Aufruferelement zugehoerige
+     *  passende Information anzeigen laesst.
+     * @pre Die verwendeten Methoden muessen verfuegbar und erreichbar sein sowie Design-By-Contract erfuellen.
+     * @post Die Info der Pane, die zu einer Klasse gehoert, wurde auf dem "CharakterDisplay" angezeigt.
+     * @param mouseEvent Das Event, das den Methodenaufruf ausgeloest hat
+     * @Author Felix Ahrens
+     */
     @FXML
     public void zeigeInfo (MouseEvent mouseEvent)
     {
