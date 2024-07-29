3d9e90ef01f8650f0022c1a47c69dce49d1cc022
EnesISL <oezcan@lenoml20m6-1.intern.isl.org>
2024-07-29 10:35:55 +0200

    .

---
+++ src/main/java/control/GameFileController.java
@@ -0,0 +1,9 @@
+package control;
+
+public class GameFileController {
+}
+/**
+ * Konzeptuell bisher aber ich denke das koennte eine gute Loesung sein.
+ * Die Spielstaende werden in Textdateien gespeichert und koennen hiervon eingelesen werden.
+ * ~Felix
+ */
